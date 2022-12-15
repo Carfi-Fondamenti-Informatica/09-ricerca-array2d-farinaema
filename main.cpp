d315555cbf4e84e51ab23df5e75d6acd1e4b772e
farinaema <117036663+farinaema@users.noreply.github.com>
2022-12-15 22:22:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,37 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
+
+    char matrice[10][20], nome[20];
+    int i, j, k;
+
+    for ( i = 0; i < 10; i++) {
+        for ( j = 0; j < 20; j++) {
+            matrice[i][j]=' ';
+        }
+    }
+
+    for ( k = 0; k < 20; k++) {
+        nome[i]=' ';
+    }
+
+    for ( i = 0; i < 10; i++) {
+        for ( j = 0; j < 20; j++) {
+            cin >> matrice[i][j];
+        }
+    }
+
+    for ( k = 0; k < 20; k++) {
+        cin >> nome[i];
+    }
+
+
+    if (miao(matrice, nome)<10){
+        cout << miao(matrice, nome) << endl;
+    } else {
+        cout << "errore" << endl;
+    }
+
 }
