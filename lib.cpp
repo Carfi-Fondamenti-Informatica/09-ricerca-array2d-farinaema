471f789f8aefa9da281c746e23a1408374aeda7c
farinaema <117036663+farinaema@users.noreply.github.com>
2022-12-15 23:27:44 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -3,14 +3,14 @@
 
 int miao(char matrice[10][20], char nome[20]){
 
-    int i=0, k=0;
+    int i=0, j=0;
 
-    while(k < 19 and i <10){
-        if (matrice[i][k]==nome[k]){
-            k++;
-        } else if (matrice[i][k]!=nome[k]) {
+    while(j < 19 and i <10){
+        if (matrice[i][j]==nome[j]){
+            j++;
+        } else if (matrice[i][j]!=nome[j]) {
             i++;
-            k=0;
+            j=0;
         }
     }
     return i;
